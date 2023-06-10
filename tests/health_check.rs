16defb3873fde4b92ceddf3d91c0ef9b56b0266f
Ben Klein <bk.romulox@protonmail.com>
2023-06-10 17:24:08 -0400

    finish chapter 6

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -123,3 +123,31 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=foobar@gmail.com", "empty name"),
+        ("name=foobar&email=", "empty email"),
+        ("name=foobar&email=notanemail", "not an email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "description: {}",
+            error_message
+        );
+    }
+}
