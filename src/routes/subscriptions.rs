4d23b9489f2b2d9ce3ae458b5738a1532320dbc6
Ben Klein <bk.romulox@protonmail.com>
2023-06-27 19:34:52 -0400

    chapter 7.7.3

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,5 @@
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
+use crate::email_client::EmailClient;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -22,21 +23,24 @@ impl TryFrom<FormData> for NewSubscriber {
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
-    skip(form, pool),
+    skip(form, pool, email_client),
     fields(
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
 )]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>, email_client: web::Data<EmailClient>) -> HttpResponse {
     let new_subscriber = match form.0.try_into() {
-        Ok(new_subscriber) => new_subscriber,
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
     }
+    if send_confirmation_email(&email_client, new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+    HttpResponse::Ok().finish()
 }
 
 #[tracing::instrument(name = "Saving new subscriber in db.", skip(new_subscriber, pool))]
@@ -61,4 +65,28 @@ async fn insert_subscriber(
         e
     })?;
     Ok(())
+}
+
+#[tracing::instrument(name = "Send confirmation email to a new subscriber",
+                      skip(new_subscriber, email_client))]
+async fn send_confirmation_email(email_client: &EmailClient, new_subscriber: NewSubscriber) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://example.com/subscriptions/confirm";
+    let plain_body =
+            &format!(
+                "Welcome to our newsletter!\n\
+                Visit {} to confirm your subscription.",
+                confirmation_link);
+    let html_body =
+            &format!(
+                "Welcome to our newsletter!<br />\
+                Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            );
+    email_client.send_email(
+        new_subscriber.email,
+            "Welcome!",
+            html_body,
+            plain_body,
+        )
+        .await
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -4,6 +4,7 @@ use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use wiremock::MockServer;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let name = "test".into();
@@ -20,6 +21,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub connection_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -37,10 +39,13 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("failed to read config");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -56,6 +61,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         connection_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
