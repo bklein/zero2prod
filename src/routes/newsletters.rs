aba9b40f3dd22c4f42e5a3eacddf0b353c565e2d
Ben Klein <bk.romulox@protonmail.com>
2023-07-11 23:35:46 -0400

    ch 9.7

---
+++ src/routes/newsletters.rs
@@ -0,0 +1,61 @@
+use actix_web::{web,HttpResponse};
+use sqlx::PgPool;
+use actix_web::ResponseError;
+use crate::routes::error_chain_fmt;
+use actix_web::http::StatusCode;
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+pub async fn publish_newsletter(_body: web::Json<BodyData>, pool: web::Data<PgPool>) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[tracing::instrument(name = "Get confirmed subsribers", skip(pool))]
+async fn get_confirmed_subscribers(pool: &PgPool) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+        )
+        .fetch_all(pool)
+        .await?;
+        Ok(rows)
+}
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,4 @@
-use crate::helpers::{spawn_app, TestApp};
+use crate::helpers::{spawn_app, ConfirmationLinks, TestApp};
 
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
@@ -22,17 +22,65 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
         }
     });
 
-    let response = reqwest::Client::new()
-        .post(&format!("{}/newletters", &app.address))
-        .json(&newsletter_request_body)
-        .send()
-        .await
-        .expect("Failed to execute request");
-
+    let response = app.post_newsletters(newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 200);
 }
 
-async fn create_unconfirmed_subscriber(app: &TestApp) {
+#[tokio::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter plain text content",
+            "html": "<p>Newsletter HTML content.</p>",
+        }
+    });
+
+    let response = app.post_newsletters(newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Newsletter plain text body.",
+                    "html": "<p>Newsletter html body.",
+                }
+            }),
+            "missing title"
+        ),
+        (
+            serde_json::json!({
+                "title": "Newsletter title"
+            }),
+            "missing content"
+        )
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = app.post_newsletters(invalid_body).await;
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Error: {}", error_message);
+    }
+}
+
+
+async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=fred&email=fred@example.com";
 
     let _mock_guard = Mock::given(path("/email"))
@@ -45,6 +93,24 @@ async fn create_unconfirmed_subscriber(app: &TestApp) {
 
     app.post_subscriptions(body.into())
         .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+    app.get_confirmation_links(&email_request)
+}
+
+async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_links = create_unconfirmed_subscriber(app).await;
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
         .error_for_status()
         .unwrap();
 }
