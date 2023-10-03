18cdcbf5c1adfd57d5cd0b1c1ac3cf74570fab6b
Ben Klein <bk.romulox@protonmail.com>
2023-10-02 20:47:17 -0400

    ch 10.6.4.8

--- src/startup.rs
+++ src/startup.rs
@@ -7,6 +7,7 @@ use sqlx::postgres::PgPoolOptions;
 
 use actix_web::dev::Server;
 use actix_web::{web, web::Data, App, HttpServer};
+use secrecy::Secret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
@@ -38,6 +39,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -65,6 +67,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -82,8 +85,12 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
     Ok(server)
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
