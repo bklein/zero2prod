208c136db973d735c23a6c8b3516131658d44a2a
Ben Klein <bk.romulox@protonmail.com>
2023-10-08 00:36:58 -0400

    move test helpers into mod

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -8,18 +8,6 @@ mod login;
 pub use login::*;
 
 #[cfg(test)]
-mod test_utils {
-    use scraper::{ElementRef, Selector};
-
-    pub fn assert_and_get_element<'a>(html: &ElementRef<'a>, element: &str) -> ElementRef<'a> {
-        let selector = Selector::parse(element);
-        assert!(selector.is_ok());
-        let selector = selector.unwrap();
-        let selection = html.select(&selector).next();
-        assert!(selection.is_some());
-        selection.unwrap()
-    }
-}
-
+mod test_helpers;
 #[cfg(test)]
-use test_utils::assert_and_get_element;
+use test_helpers::assert_and_get_element;
